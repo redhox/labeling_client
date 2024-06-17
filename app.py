4564252c377241351baeb912709f7e2684e1b8ac
Votre Nom <Vous@exemple.com>
2024-06-18 00:26:26 +0200

    ça marche mais c'est moche

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 import uuid
 from bson import json_util
-
+from PIL import Image
+import io
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECTRET_KEY')
 SERVER_URL = os.environ.get("SERVER_URL")
@@ -29,7 +30,7 @@ if os.getenv('UUID_MACHINE') == None:
 
 UUID_MACHINE=os.getenv('UUID_MACHINE')
 def get_token(email, password):
-    url = 'http://192.168.132.12:8002/users/token'
+    url = f'{SERVER_URL}/users/token'
     data = {'email': email, 'password': password}
     headers = {'Content-Type': 'application/json'}
     try:
@@ -49,12 +50,19 @@ def get_current_user():
             'Authorization': f'Bearer {token}',
         }
         print('headers',headers,f'url {SERVER_URL}/users/protected_route')
-        response = requests.post(
-            f'{SERVER_URL}/users/protected_route', headers=headers)
+        response = requests.post(f'{SERVER_URL}/users/protected_route', headers=headers)
+
+        # headers = {'Authorization': f'Bearer {token}'}
+        # response = requests.get(f'{SERVER_URL}/users/protected_route', headers=headers)
+
         if response.status_code == 200:
             user_data = response.json()
             return user_data
+        if response.status_code == 401:
+                print("Authentication failed")
+                return logout(),401
     return login()
+
 def check_authentication(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -62,23 +70,32 @@ def check_authentication(func):
         if request.endpoint == 'predict_route':
             # Extract the token from the request's form
             token = request.form['token']
-        else:
-            # Check for the token in the session
-            token = session.get('token')
-        if token:
-            # Verify the token using the get_token function
             headers = {'Authorization': f'JWT {token}'}
-            response = requests.get(f'{SERVER_URL}/users/protected_route', headers=headers)
+            response = requests.post(f'{SERVER_URL}/users/protected_route', headers=headers)
             if response.status_code == 200:
                 # Do not print the result here
                 pass
-            else:
-                print(
-                    f"Failed to access protected resource. Status code: {response.status_code}")
-            return func(*args, **kwargs)
+            if response.status_code == 401:
+                print("Authentication failed")
+                return logout(),401
         else:
-            print("Authentication failed")
-            return login(),401
+            # Check for the token in the session
+            token = session.get('token')
+            if token:
+                # Verify the token using the get_token function
+                headers = {'Authorization': f'JWT {token}'}
+                response = requests.post(f'{SERVER_URL}/users/protected_route', headers=headers)
+                if response.status_code == 200:
+                    # Do not print the result here
+                    pass
+                else:
+                    print(f"Failed to access protected resource. Status code: {response.status_code}")
+                    
+                return func(*args, **kwargs)
+            else:
+                print("Authentication failed")
+                return logout(),401
+        
     return wrapper
 
 def compter_elements_dans_dossier(dossier):
@@ -89,7 +106,7 @@ def dir_in_dir(path):
     dir_list=[]
     for racine, sous_dossiers, fichiers in sorted(os.walk(path)):
         for sous_dossier in sous_dossiers:
-            print(os.path.join(racine, sous_dossier))
+            print("dir_in_dir",os.path.join(racine, sous_dossier))
             image_number = compter_elements_dans_dossier(f'{racine}/{sous_dossier}')
             dir_list.append([sous_dossier,image_number])
     return dir_list
@@ -101,27 +118,22 @@ def images_in_dir(path):
     for racine, soudoss, fichiers in sorted(os.walk(path)):
         for fichier in fichiers:
             if fichier != "integrity.check" or "metadata.json":
-                image_list.append([fichier])
+                image_list.append(fichier)
     return image_list
 
-
+ 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    print("bonjour")
-    if request.method == 'POST': 
-        print("post")
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        print('de rretour dans app ',email)
         token = get_token(email, password)
-        print('j ais le tocken',token)
         if token:
             session['token'] = token  # Store the token in the session
             return redirect(url_for('dashboard'))
         else:
             return jsonify({'message':'auth fail'})
     
-    print("get")
     if session.get('token'):
         return redirect(url_for('dashboard'))
     return render_template('login.html')
@@ -130,22 +142,23 @@ def login():
 @app.route('/dashboard')
 @check_authentication
 def dashboard():
-    token = session.get('token')
+    token = session.get('token') 
     headers = {'Authorization': f'JWT {token}'}
     user=get_current_user()
     print("dashboard")
+    print("dashboard",user)
     return render_template('dashboard.html',token=token , user=user)
-    
+     
 
 
-@app.route('/labelling/<pathname>/<dirname>/<filename>', methods=['GET'])
-@check_authentication
-def labelling(pathname,dirname,filename):
+@app.route('/labelling/<pathname>/<dirname>/<filename>/<string:model>', methods=['GET'])
+@check_authentication 
+def labelling(pathname,dirname,filename,model):
     token = session.get('token')
-    if token: 
+    user=get_current_user()
+    if token:
 
         filepath=[pathname,dirname,filename]
-        print(f'/{filepath[0]}/{filepath[1]}/{filepath[2]}')
         with open(f'/{filepath[0]}/{filepath[1]}/{filepath[2]}', "rb") as img_file:
             files = {'file': img_file}
         
@@ -156,33 +169,73 @@ def labelling(pathname,dirname,filename):
                 f"{SERVER_URL}/images/image_search", 
                 data={"path": f'{UUID_MACHINE}/{filepath[1]}/{filepath[2]}'}, 
                 )
-            
+            label_confirm=''
+
             if response.status_code == 200:
                 response_data = response.json()
                 data = json.loads(response_data)
-                regions = data['regions'] 
-                
+                print('data status 200=',data)
+                try:
+                    regions = data['regions'] 
+                    liste_objet=[]
+                    for region in regions:
+                        liste_objet.append(region['region_attributes']['objet'])   
+                except:
+                    regions=[] 
+                    liste_objet=[]
+                 
+                label_confirm='verified by human'
+
             else:
-                # envoi de limage
-                response = requests.post(
+                # envoi de limage 
+                response = requests.post( 
                     f"{SERVER_URL}/images/image_save",
-                    data={"path": f'{UUID_MACHINE}/{filepath[1]}/{filepath[2]}'},
+                    data={"path": f'{UUID_MACHINE}/{filepath[1]}/{filepath[2]}',"model":model},
                     files=files
                     )
                 response_data = response.json()
                 regions = json.dumps(response_data)
-                # Load the JSON data
+                # Load the JSON data 
                 data_dict = json.loads(regions)
+                
+                # Extract the information 
+                print('data from model=',data_dict)
+                try:
+                    regions = data_dict['regions'] 
+                except:
+                    regions=[]
+                print('region=', regions )
+                try:
+                    liste_objet_data = data_dict['liste_objet'] 
+                    liste_objet=[]
+                    for element in liste_objet_data:
+                        liste_objet.append(liste_objet_data[element])
+                except:
+                    liste_objet=[]
+                print('listeobjet',liste_objet)
 
-                # Extract the information
-                regions = data_dict["regions"]
-            print('region=', regions )
             # regions=response_data
-        
-        image = filename
-        liste_images=images_in_dir(f'/{filepath[0]}/{filepath[1]}')
-        print('liste region',regions)
-        return render_template('labelling.html', regions=regions, filepath=filepath,liste_images=liste_images, filesize=440 )
+                
+        # creation de la liste thumbnail
+        liste_images=images_in_dir(f'/{filepath[0]}/{filepath[1]}') 
+        indice_element = liste_images.index(filename)
+        avant = liste_images[max(indice_element - 3, 0):indice_element]
+        apres = liste_images[indice_element + 1:min(indice_element + 4, len(liste_images))]
+        liste_images = avant + [filename] + apres
+        try :
+            nextimage=apres[0]
+        except:
+            nextimage=False
+
+        index_in_list=[indice_element,len(liste_images)]
+        response = requests.post(f"{SERVER_URL}/models/actif_model")
+        response_data = response.json()
+        data = json.loads(response_data) 
+        # print('listemodel= ',data)
+        lists_model=data
+
+
+        return render_template('labelling.html',nextimage=nextimage,index_in_list=index_in_list, regions=regions, filepath=filepath,liste_images=liste_images, filesize=440 ,model=model ,liste_models=lists_model,liste_objet=liste_objet,label_confirm=label_confirm,user=user)
     else:
         print("Authentication failed")
         return jsonify({'message': 'Authentication failed'}), 401
@@ -191,7 +244,7 @@ def labelling(pathname,dirname,filename):
 def resultat():
     print("resultat")
     token = session.get('token')
-
+ 
     if token:
         headers = {'Authorization': f'JWT {token}'}
         file = request.files['file']
@@ -210,7 +263,7 @@ def resultat():
             path=f"{UUID_MACHINE}/{filedir}/{filename}"
             regions=data[first_key]["regions"]
             data = {'filename':filename,"filedir":filedir,"path":path,'regions': regions,"uuid_machine":UUID_MACHINE}
-            print(data)
+            print("resultat",data)
             response = requests.post(f'{SERVER_URL}/images/post_resultat', json=data, headers=headers)
             return jsonify({"message": " File uploaded successfully"}), 200 
 
@@ -219,17 +272,27 @@ def get_image(pathname,dirname,filename):
     print("getimage")
     image_path = f'/{pathname}/{dirname}/{filename}'
     with Image.open(image_path) as img:
-        image_format = img.format.lower()
-        return send_file(image_path, mimetype=f'.{image_format}')
+        return send_file(image_path)
 
-
+    
+@app.route('/get_image_thumbnail/<pathname>/<dirname>/<filename>' ,methods=['GET'])
+def get_image_thumbnail(pathname,dirname,filename):
+    print("getimage")
+    image_path = f'/{pathname}/{dirname}/{filename}'
+    with Image.open(image_path) as img:
+        new_size = (170, 170)  # Exemple : 100px de large et 100px de haut
+        img.thumbnail(new_size)
+        buffered = io.BytesIO()
+        img.save(buffered, format="JPEG")
+        return send_file(io.BytesIO(buffered.getvalue()), mimetype='image/jpeg')
+    
 @app.route('/overview')
 @check_authentication
 def overview():
     user=get_current_user()
     images = dir_in_dir('./dossier_images')
     
-    return render_template('overview.html', images=images, username=user)
+    return render_template('overview.html', images=images, user=user)
 
 @app.route('/overview/<dirname>')
 @check_authentication
@@ -237,121 +300,63 @@ def overview_dir(dirname):
     user=get_current_user()
     images = images_in_dir(f'/image_dir/{dirname}')
     path=f'image_dir'
-    return render_template('overview_img.html', images=images,path=path,dirname=dirname, username=user)
-
-# @app.route('/admin/user_management')
-# @check_authentication
-# def user_management():
-#     token = session.get('token')
-
-#     if token:
-#         headers = {'Authorization': f'JWT {token}'}
-#         user_id = requests.post(f'{SERVER_URL}/user_name', headers=headers)
-#         username = user_id.json()['username']
-#         role = user_id.json()['role']
-#         try:
-#             # Remplacez l'URL par l'URL r�elle de votre API pour r�cup�rer les utilisateurs
-#             response = requests.post(f'{SERVER_URL}/all_user', headers=headers)
-#             response.raise_for_status()  # G�re les erreurs HTTP
-#             users = response.json()  # Convertit la r�ponse JSON en Python dict
-#             print('users=',users)
-#             return render_template('user_management.html', users=users, username=username, role=role)
-#         except requests.exceptions.RequestException as e:
-#             print(f"Error fetching users: {e}")
-#             return []
-#     else:
-#         print("Authentication failed")
-#         return jsonify({'message': 'Authentication failed'}), 401
+    model='model_non_actif'
+    return render_template('overview_img.html', images=images,path=path,dirname=dirname, user=user,model=model)
 
 
-# @app.route('/admin/register', methods=['GET', 'POST'])
-# @check_authentication
-# def register():
-#     if request.method == 'POST':
-#         # Rest of your registration code remains unchanged
-#         admin_token = session.get('token')
-#         if admin_token:
-#             # Cr�ez un nouvel utilisateur avec les donn�es minimales
-#             username = request.form['username']
-#             password = request.form['password']
-#             role = request.form['role']
-
-#             headers = {'Authorization': f'JWT {admin_token}'}
-#             data = {
-#                 'username': username,
-#                 'password': password,
-#                 'role': role
-#             }
-
-#             try:
-#                 response = requests.post(
-#                     f'{SERVER_URL}/create_user', headers=headers, json=data)
-#                 response.raise_for_status()
-
-#                 if response.status_code == 201:
-#                     flash('User created successfully', 'success')
-#                     return redirect(url_for('user_management'))
-#                 else:
-#                     error_message = response.json().get('message')
-#                     flash(
-#                         f"User creation failed. Status code: {response.status_code}, Message: {error_message}", 'error')
-
-#             except requests.exceptions.RequestException as e:
-#                 flash(f"Error during user creation: {e}", 'error')
-
-#         else:
-#             flash("Admin authentication failed", 'error')
-#             return redirect(url_for('user_management'))
-
-#     return redirect(url_for('user_management'))
 
 
-# @app.route('/admin/delete_user/<int:user_id>', methods=['POST'])
-# @check_authentication
-# def delete_user(user_id):
-#     token = session.get('token')
-#     if token:
-#         try:
-#             headers = {'Authorization': f'JWT {token}'}
-#             data = {'id':user_id}
-#             response = requests.post(f'{SERVER_URL}/delete_user',json=data , headers=headers)
-#             response.raise_for_status()
-#             return user_management(), 200
-#         except requests.exceptions.RequestException as e:
-#             print(f"Error deleting user: {e}")
-#             return jsonify({'message': f'Erreur lors de la suppression de l\'utilisateur'}), 500
-#     else:
-#         return jsonify({'message': f'L\'authentification a �chou�'}), 401
+@app.route('/model_list')
+@check_authentication
+def model_list():
+    user=get_current_user()
+    if user['is_admin'] != True:
+        return dashboard() 
+    response = requests.post(f'{SERVER_URL}/models/lists_models')
+    response_data = response.json()
+    list_model_all = json.loads(response_data)
+    print('model data',list_model_all)
+
+    response = requests.post(f'{SERVER_URL}/models/actif_model')
+    response_data = response.json()
+    list_model_actif = json.loads(response_data)
+    # list_model_actif = json.dumps(response_data)
+
+    return render_template('model_dashboard.html', list_model_actif=list_model_actif,list_model_all=list_model_all,user=user)
+
+@app.route('/add_model_list/<string:id_model>', methods=['GET'])
+@check_authentication
+def add_model_list(id_model):
+    print('id model',id_model)
+    response = requests.post(f'{SERVER_URL}/models/add_model_id', json={'id':id_model})
+    response_data = response.json()
+    return json.dumps({"message": "Bonjour"})
+
+@app.route('/del_model_list/<string:id_model>', methods=['GET'])
+@check_authentication
+def del_model_list(id_model):
+    print('id model',id_model)
+    response = requests.post(f'{SERVER_URL}/models/del_model_id', json={'id':id_model})
+    response_data = response.json()
+    print('response_data del model',response_data)
+    return json.dumps({"message": "Bonjour"})
+
+@app.route('/liste_actif_model') 
+@check_authentication
+def liste_actif_model():
+    response = requests.post(f'{SERVER_URL}/models/actif_model')
+    response_data = response.json()
+    list_model_actif = json.loads(response_data)
+    print('liste model',list_model_actif)
+    return list_model_actif 
 
 
-# @app.route('/admin/switch_role/<int:user_id>', methods=['POST'])
-# @check_authentication
-# def switch_role(user_id):
-#     token = session.get('token')
-#     if token:
-#         try:
-#             headers = {'Authorization': f'JWT {token}'}
-#             data = {'id':user_id}
-#             response = requests.post(f'{SERVER_URL}/user',json=data , headers=headers)
-#             response.raise_for_status()
-#             user_data = response.json().get('user')
-#             current_role = user_data.get('role')
-#             new_role = 'admin' if current_role == 'utilisateur' else 'utilisateur'
-#             data = {'id':user_id,'role': new_role}
-#             response = requests.post(f'{SERVER_URL}/change_role', json=data, headers=headers)
-#             response.raise_for_status()
-#             return user_management(), 200
-#         except requests.exceptions.RequestException as e:
-#             print(f"Error switching role: {e}")
-#             print(f"Response content: {response.content}")
-#     else:
-#         return jsonify({'message': 'L\'authentification a �chou�'}), 401
 
 
-@app.route('/logout')
+@app.route('/logout') 
 def logout():
     session.clear()
-    return redirect(url_for('login'))
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
